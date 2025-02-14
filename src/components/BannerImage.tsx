d1c2f526329c30a10e82aac9edfb78d3b9f34642
MatheusPriv <matheus.engsof@outlook.com>
2025-02-13 22:13:06 -0300

    Feat material-ui-insert

---
+++ src/components/BannerImage.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const BannerImage = styled.div`
+  background-image: url(/Login-image.svg);
+  background-size: cover;
+  height: 100vh;
+  width: 50vw;
+`
